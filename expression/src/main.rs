12fac2e7419bbe053042991c88dfb710e9acf719
kdnakt <a.kid.1985@gmail.com>
2026-02-15 11:48:20 +0900

    expression

---
+++ expression/src/main.rs
@@ -0,0 +1,123 @@
+/// 2 つのサブ式に対して実行する演算。
+#[derive(Debug)]
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+/// ツリー形式の式。
+#[derive(Debug)]
+enum Expression {
+    /// 2 つのサブ式に対する演算。
+    Op { op: Operation, left: Box<Expression>, right: Box<Expression> },
+
+    /// リテラル値
+    Value(i64),
+}
+
+fn eval(e: Expression) -> Result<i64, String> {
+    match e {
+        Expression::Value(n) => Ok(n),
+        Expression::Op { op, left, right } => {
+            let l = eval(*left)?;
+            let r = eval(*right)?;
+            match op {
+                Operation::Add => Ok(l + r),
+                Operation::Sub => Ok(l - r),
+                Operation::Mul => Ok(l * r),
+                Operation::Div => {
+                    if r == 0 {
+                        Err(String::from("division by zero"))
+                    } else {
+                        Ok(l / r)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn test_value() {
+    assert_eq!(eval(Expression::Value(19)), Ok(19));
+}
+
+#[test]
+fn test_sum() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(Expression::Value(10)),
+            right: Box::new(Expression::Value(20)),
+        }),
+        Ok(30)
+    );
+}
+
+#[test]
+fn test_recursion() {
+    let term1 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Value(10)),
+        right: Box::new(Expression::Value(9)),
+    };
+    let term2 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Op {
+            op: Operation::Sub,
+            left: Box::new(Expression::Value(3)),
+            right: Box::new(Expression::Value(4)),
+        }),
+        right: Box::new(Expression::Value(5)),
+    };
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(term1),
+            right: Box::new(term2),
+        }),
+        Ok(85)
+    );
+}
+
+#[test]
+fn test_zeros() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(Expression::Value(0)),
+            right: Box::new(Expression::Value(0))
+        }),
+        Ok(0)
+    );
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Mul,
+            left: Box::new(Expression::Value(0)),
+            right: Box::new(Expression::Value(0))
+        }),
+        Ok(0)
+    );
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Sub,
+            left: Box::new(Expression::Value(0)),
+            right: Box::new(Expression::Value(0))
+        }),
+        Ok(0)
+    );
+}
+
+#[test]
+fn test_error() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Div,
+            left: Box::new(Expression::Value(99)),
+            right: Box::new(Expression::Value(0)),
+        }),
+        Err(String::from("division by zero"))
+    );
+}
